70eaf5791090d172041eec61fa7f022fb8829e4b
Sahil-Athalye <a.sahil@wustl.edu>
2023-10-15 04:52:07 -0500

    adding vue and nuxt

---
+++ vue/pages/index.vue
@@ -0,0 +1,29 @@
+
+
+<script setup>
+
+import OpenAI from "openai";
+import {ref} from 'vue'
+
+let resp = ref("")
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const chatCompletion = await openai.chat.completions.create({
+    messages: [{ role: "user", content: "Say this is a test" }],
+    model: "gpt-3.5-turbo",
+});
+
+resp.value = chatCompletion.choices[0].message.content
+
+</script>
+
+
+<template>
+    {{resp}}
+</template>
+
+<style> 
+</style>
