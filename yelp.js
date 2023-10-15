c0a0c416f15b8552a5b7c438cd4cdeea8b8310fc
Sahil-Athalye <a.sahil@wustl.edu>
2023-10-15 04:44:02 -0500

    null changes

--- yelp.js
+++ yelp.js
@@ -1,29 +1,36 @@
 const apikey = "Bearer uiXTjOetV4sMDc4azxklccywMklJueCLj071X6Wii7lnOLr-7WBjn7Om1DXQTibS78ryOn3bMb8rsCKNWBeD3ccaiWugRttPSTVYJxE6o0D1R2rp2oXBlzcROd4qZXYx"
 
+document.getElementById("submitFlavors").addEventListener("click", displayDish, false);
 document.getElementById("submitbutton").addEventListener("click", getUserInput, false);
 
+let starch = false;
+let sweet = false;
 
 function getUserInput(){   
     // Setting variable for form input (get from HTML form)
     var location = document.getElementById("city").value;
     document.getElementById("city").value = "";
 
-    var term = "food"; // change this to cuisine drop down
+    var cuisine = document.getElementById("cuisine").value;
+    var rating = parseFloat(document.getElementById("fader").value);
+    var price = parseInt(document.getElementById("fader1").value);
+    var reviews = parseInt(document.getElementById("fader2").value);
+    var radius = parseInt(document.getElementById("fader3").value) * 1600;
 
-    var rating = document.getElementById("fader").value;
-    var price = document.getElementById("fader1").value;
-    var reviews = document.getElementById("fader2").value;
-    var radius = parseInt(document.getElementById("fader4").value) * 1600;
+    // checkboxes
+    // var reservation = document.getElementById("").value;
+    // var outdoor_seating = document.getElementById("").value;
+    // var wheelchair_accessible = document.getElementById("").value;
+    // var open_now = document.getElementById("").value;
+    // var veg_friendly = document.getElementById("").value;
 
-    console.log(location);
-    console.log(rating);
-    console.log(price);
-    console.log(reviews);
-    console.log(radius);
 
-    let url = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location="+location+"&term=indian&radius="+radius+"&categories=&sort_by=rating&limit=50";
+    //let url = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location="+location+"&term="+cuisine+"&radius="+radius+"&open_now="+open_now+"&attributes="+reservation+"&attributes="+outdoor_seating+"&attributes="+wheelchair_accessible+"attributes="+veg_friendly+"&categories=&sort_by=rating&limit=50";
+    let url = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location="+location+"&term="+cuisine+"&radius="+radius+"&categories=&sort_by=rating&limit=50";
+
 
     getRestaurants(url, reviews, price, rating);
+    displayQuestionnaire();
 
 }
 
@@ -52,15 +59,47 @@ function getRestaurants(url, reviews, price, rating){
                 break;
             }
             else {
-                if ((restaurants[i].review_count >= reviews) && (restaurants[i].rating >= rating) && (restaurants[i].price >= price)){
-                    filtered[j] = restaurants[i];
-                    j++;
+                if (('review_count' in restaurants[i]) && ('rating' in restaurants[i]) && ('price' in restaurants[i])){
+                    var newPrice = 1;
+                    if (restaurants[i].price == "$"){
+                        newPrice = 1;
+                    }
+                    else if (restaurants[i].price == "$$"){
+                        newPrice = 2;
+                    }
+                    else if (restaurants[i].price == "$$$"){
+                        newPrice = 3;
+                    }
+                    else {
+                        newPrice = 4;
+                    }
+
+                    if ((restaurants[i].review_count >= reviews) && (restaurants[i].rating >= rating) && (newPrice >= price)){
+                        filtered[j] = restaurants[i];
+                        j++;
+                    }             
                 }
-                i++;
             }
+            i++;
         }
 
         console.log(filtered);
+
+        // show restaurants in table
+        for (var k = 0; k < filtered.length; k++){
+            var num = k+1;
+
+            var link = document.createElement("a");
+            link.setAttribute("href", filtered[k].url);
+
+            var linkText = document.createTextNode(filtered[k].name);
+            link.appendChild(linkText);
+
+            document.getElementById("restaurant"+num).appendChild(link);
+            document.getElementById("rating"+num).innerHTML = filtered[k].rating;
+            document.getElementById("reviews"+num).innerHTML = filtered[k].review_count;
+        }
+        
     });
 }
 
@@ -78,62 +117,125 @@ const eastAsia = ["Chinese", "Japanese", "Korean"];
 const vietnam = ["Vietnamese"];
 const seAsia = ["Thai", "Cambodian", "Lao", "Philippines"];
 
-$.ajax({
-  type: "POST",
-  url: "./backend/celebrity.py",
-  data: { param: text},
-  dataType: "text",
-}).done(function( o ) {
-   dish = get_chatgpt_response(input_string);
-});
-
 function displayQuestionnaire(){
 
-  document.getElementById("spiceQuestion").classList.remove("invisible");
-  document.getElementById("vegQuestion").classList.remove("invisible");
-  document.getElementById("meatQuestion").classList.remove("invisible");
-
-  cuisine = document.getElementById("cuisine").value;
-  if(eastAfrica.includes(cuisine)){
-
-  }
-  else if(eastAsia.includes(cuisine)){
-    document.getElementById("starchQuestion").classList.remove("invisible");
-  }
-  else if(vietnam.includes(cuisine)){
-
-  }
-  else if(seAsia.includes(cuisine)){
-    document.getElementById("sweetQuestion").classList.remove("invisible");
-  }
-  else if(westAfrica.includes(cuisine)){
-
-  }
-  else if(indianSubCont.includes(cuisine)){
-
-  }
-  else if(centralAmerica.includes(cuisine)){
-
-  }
-  else if(caribbean.includes(cuisine)){
-
-  }
-  else if(southAmerica.includes(cuisine)){
-
-  }
-  else if(westEurope.includes(cuisine)){
-
-  }
-  else if(Italy.includes(cuisine)){
-
-  }
-  else if(ottoman.includes(cuisine)){
-
-  }
-  else if(middleEast.includes(cuisine)){
-
+    document.getElementById("allergyQuestion").classList.remove("invisible");
+  
+    document.getElementById("meatQuestion").classList.remove("invisible");
+  
+    document.getElementById("spiceQuestion").classList.remove("invisible");
+  
+    
+    cuisine = document.getElementById("cuisine").value;
+    
+    if(eastAsia.includes(cuisine)){
+      document.getElementById("starchQuestion").classList.remove("invisible");
+      starch = true;
+      document.getElementById("sweetQuestion").classList.add("invisible");
+    }
+    else if(seAsia.includes(cuisine)){
+      document.getElementById("sweetQuestion").classList.remove("invisible");
+      sweet = true;
+      document.getElementById("starchQuestion").classList.add("invisible");
+    }
+    else{
+      document.getElementById("sweetQuestion").classList.add("invisible");
+      document.getElementById("starchQuestion").classList.add("invisible");
+      starch = false;
+      sweet = false;
+    }
+    // else if(vietnam.includes(cuisine)){
+      
+    // }
+    // else if(eastAfrica.includes(cuisine)){
+  
+    // }
+    // else if(westAfrica.includes(cuisine)){
+  
+    // }
+    // else if(indianSubCont.includes(cuisine)){
+  
+    // }
+    // else if(centralAmerica.includes(cuisine)){
+  
+    // }
+    // else if(caribbean.includes(cuisine)){
+  
+    // }
+    // else if(southAmerica.includes(cuisine)){
+  
+    // }
+    // else if(westEurope.includes(cuisine)){
+  
+    // }
+    // else if(Italy.includes(cuisine)){
+  
+    // }
+    // else if(ottoman.includes(cuisine)){
+  
+    // }
+    // else if(middleEast.includes(cuisine)){
+  
+    // }
+    
   }
   
-
-
-}
+  function displayDish(){
+    const input_strings = [];
+  
+    //allergy input
+    if(document.getElementById('allergyInput').value==""){
+      input_strings.push("null");
+    }
+    else{
+      input_strings.push(document.getElementById('allergyInput').value);
+    }
+  
+    //meat choice input
+    if(document.getElementById('Beef').checked) {
+      input_strings.push("beef");
+    }
+    if(document.getElementById('Pork').checked) {
+      input_strings.push("pork");
+    }
+    if(document.getElementById('Chicken').checked) {
+      input_strings.push("chicken");
+    }
+    else{
+      input_strings.push("vegetarian");
+    }
+  
+    //spice input
+    if(document.getElementById('spiceYes').checked) {
+      input_strings.push("spicy");
+    }
+    else{
+      input_strings.push("");
+    } 
+  
+    if(document.getElementById('spiceYes').checked) {
+      input_strings.push("spicy");
+    }
+    else{
+      input_strings.push("");
+    } 
+  
+    if(sweet){
+      if(document.getElementById('sweetYes').checked) {
+        input_strings.push("sweet");
+      }
+      else{
+        input_strings.push("");
+      } 
+    }
+  
+    if(starch){
+      if(document.getElementById('Rice').checked) {
+        input_strings.push("rice");
+      }
+      else{
+        input_strings.push("noodles");
+      } 
+    }
+  
+  }
